28f6491ed5877ca218d34be8ff47ea07e9ba8f08
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2021-02-04 10:12:18 +0200

    include: dt-bindings: add ds90ub9xx.h (for GPIOs)

---
+++ include/dt-bindings/media/ds90ub9xx.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/**
+ * Definitions for the Texas Instruments DS90UB9xx FPDLink Serializers and Deserializers
+ *
+ * Copyright (c) 2019 Luca Ceresoli <luca@lucaceresoli.net>
+ * Copyright (c) 2021 Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
+ */
+
+#ifndef _DS90UB9XX_H
+#define _DS90UB9XX_H
+
+#define DS90_GPIO_FUNC_UNUSED		0
+#define DS90_GPIO_FUNC_OUTPUT		1
+#define DS90_GPIO_FUNC_INPUT		2
+#define DS90_GPIO_FUNC_OUTPUT_REMOTE	3
+#define DS90_GPIO_N_FUNCS		4
+
+#endif /* _DS90UB9XX_H */
